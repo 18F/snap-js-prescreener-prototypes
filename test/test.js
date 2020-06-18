f92fbcd9bd2eb5300fefd3f50479cf8f81544c21
Alex Soble <alex.soble@gsa.gov>
2020-06-18 15:02:42 -0500

    Add mocha and chai for assertions

--- test/test.js
+++ test/test.js
@@ -1,27 +1,38 @@
 const puppeteer = require('puppeteer');
+const assert = require('chai').assert;
 
-(async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    const file_url = 'file:///Users/alexrsoble/hs-apis/snap-js-prescreener-prototypes/prescreeners/va.html';
+describe('async', () => {
+    it('should have correct expectation', () => {
 
-    await page.goto(file_url);
+        (async () => {
+            const browser = await puppeteer.launch();
+            const page = await browser.newPage();
+            const file_url = 'http://localhost:8081/prescreeners/va.html';
 
-    await page.select('#household_size', '1');
-    await page.click('label[for="input__household_includes_elderly_or_disabled_false"]');
+            await page.goto(file_url);
+            await page.select('#household_size', '1');
+            await page.click('label[for="input__household_includes_elderly_or_disabled_false"]');
+            await page.click('label[for="input__all_citizens_question_true"]');
+            await page.type('#monthly_job_income', '0');
+            await page.type('#monthly_non_job_income', '0');
+            await page.type('#resources', '0');
+            await page.click('#prescreener-form-submit');
+            await page.screenshot({ fullPage: true, path: 'screenshot.png' });
 
-    await page.type('#monthly_job_income', '0');
-    await page.type('#monthly_non_job_income', '0');
-    await page.type('#resources', '0');
+            await page.waitForSelector('.result-headline', {
+                'visible': true,
+                'timeout': 5000
+            });
 
-    await page.click('#prescreener-form-submit');
-    await page.screenshot();
+            const innerText = await page.evaluate(() => document.querySelector('#results').innerText)
+            const expectedInnerText = `Results:
+                You may be eligible for SNAP benefits.
+                If approved, your benefit could be as much as $194 per month.
+                Apply here: https://commonhelp.virginia.gov/.`;
 
-    page
-        .waitForSelector('#result-headline', {'visible': true});
+            assert.equal(innerText.replace(/\s+/g, ''), expectedInnerText.replace(/\s+/g, ''));
 
-    const html = await page.content();
-    console.log(html);
-
-    // await browser.close();
-})();
+            await browser.close();
+        })();
+    });
+});
