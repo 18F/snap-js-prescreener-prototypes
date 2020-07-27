150278cb8ed7a866a6cbab4c2840b8c7b3d78ce5
Alex Soble <alex.soble@gsa.gov>
2020-07-27 17:59:19 -0500

    Handle unchecked/empty radio buttons

--- prescreeners/form-controls.js
+++ prescreeners/form-controls.js
@@ -142,7 +142,10 @@
                         jsonData[elem.id] = elem.value;
                         break;
                     case 'radio':
-                        jsonData[elem.name] = document.querySelector(`input[name="${elem.name}"]:checked`).value;
+                        let checked = document.querySelector(`input[name="${elem.name}"]:checked`);
+                        (checked)
+                            ? jsonData[elem.name] = checked.value
+                            : null;
                         break;
                     case 'text':
                         jsonData[elem.id] = elem.value;
